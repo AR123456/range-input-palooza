8f517ea29768021c1c4c8f7adddb892e9be7c759
AR123456 <coloradoz99@gmail.com>
2022-12-31 19:00:44 -0700

    cleaning up code

--- range-ticks-progress-labels-fancy/appfancy.js
+++ range-ticks-progress-labels-fancy/appfancy.js
@@ -1,6 +1,3 @@
-//TODO create a app that will take in a dollar amount goal and years goal
-// the app will calculate how much needs to be saved per month to achieve  the goal
-// getting range and text inputs
 const sliderGoal = document.getElementById("savingsRange");
 const inputGoal = document.getElementById("savingsInput");
 const sliderYears = document.getElementById("yearsRange");
@@ -13,16 +10,9 @@ const sliderExpectedRate = document.getElementById("expectedRateRange");
 const inputExpectedRate = document.getElementById("expectedRateInput");
 const sliderInflationRate = document.getElementById("expectedInflationRange");
 const inputInflationRate = document.getElementById("expectedInflationInput");
-const actualTime = document.getElementById("actualTime");
-// Buttons
-const calculateButton = document.getElementById("calculate");
-const viewReportButton = document.getElementById("viewReport");
 
-// function to sync range and  text box inputs
+// function to sync range and text box inputs
 function syncInputs() {
-  // https://stackoverflow.com/questions/64199456/changing-the-value-of-the-range-slider-and-input-box-at-the-same-time
-
-  // /////////////////////////
   sliderGoal.addEventListener("input", function () {
     inputGoal.value = this.value;
   });
@@ -68,108 +58,30 @@ function syncInputs() {
   });
 }
 syncInputs();
-// calculate  function
-const calculate = () => {
-  goal = parseFloat(inputGoal.value);
-  years = parseFloat(inputYears.value);
-  currentSaved = parseFloat(inputCurrentSaved.value);
-  monthlySaved = parseFloat(inputMonthlySavings.value);
-  expectedReturn = parseFloat(inputExpectedRate.value);
-  expectedInflation = parseFloat(inputInflationRate.value);
-  console.log(goal);
-  console.log(years);
-  console.log(currentSaved);
-  console.log(monthlySaved);
-  console.log(expectedReturn);
-  console.log(expectedInflation);
-  // real interest rate = interest rate - inflation rate
-  const realInt = (expectedReturn - expectedInflation) / 100;
-  console.log(realInt);
-  // calcTime =
-  //   Math.log(1 + (goal * realInt) / monthlySaved) / Math.log(1 + realInt);
-  //calcTime is the actual amount of time needed to reach the savings goal////
-  calcTime =
-    Math.log(1 + (goal * realInt) / (monthlySaved + currentSaved)) /
-    Math.log(1 + realInt);
-  console.log(`Months needed ${calcTime}`);
-  // https://stackoverflow.com/questions/39275225/how-to-convert-a-number-of-months-into-months-and-years
-  // act Years and actMonths are for formatting year month style
-  const actYears = Math.floor(calcTime / 12);
-  // let actMonths = Math.ceil(calcTime % 12);
-  const actMonths = Math.round(calcTime % 12);
-
-  /////
-  if (calcTime < years * 12) {
-    // to do change this to __ years and __ months
-    actualTime.innerHTML = `<span>You will reach your savings goal in ${actYears} years ${actMonths} months.</span>
-  </span>`;
-  } else {
-    actualTime.innerHTML = `<span>${years} years is not enough time. You will need ${actYears} years ${actMonths} months to reach your savings goal. </span>`;
-  }
-
-  // calcMonthly is what actually needs to be saved per month to get to goal//////////
-  // calcMonthly =( goal*realInt)/( (1+realInt) to the power of years*12)-1)
-  // Math.pow(1 + realInt,years*12)
-  calcMonthly = (goal * realInt) / (Math.pow(1 + realInt, years * 12) - 1);
-  console.log(
-    `What actually needs to be saved monthly to get to goal in year specified ${calcMonthly}`
-  );
-  //
-  //TODO calculate the value of the annuity each year for the graph.  Will need the calculated values and monthly savings for the entered values and the calculated value for what is needed as an actual monthly deposit to meet the savings goal.
-  overTimePerYearActual = "actual value of annuity";
-  overTimePerYearNeeded =
-    "what actually needs to be saved per month based on entered goal";
-};
-
-calculateButton.onclick = function () {
-  calculate();
-};
-
-// adding JS to get the range track and ticks working
-///////////////////////////////////////////////////////////////////////
-// looking at using Javascript to generate slider marks
-// https://codepen.io/viestursm/pen/BayEjaN
+// JS to get the range track and ticks working
 function init() {
-  // getting slider to pass into the functions that control appearance and behavior
-  // each range input has the class tick-slider-input
   const sliders = document.getElementsByClassName("tick-slider-input");
-
   // when the page loads use the default values to set up sliders
   for (let slider of sliders) {
     // add event listener
     slider.oninput = onSliderInput;
-
     updateProgress(slider);
-    // the slider is not in the onInput because it is defined on page load and doesnt change after that.
     setTicks(slider);
   }
 }
-// when range handle is moved pass in that event to update the slider
 function onSliderInput(event) {
-  //event.target is input id  name then append the class name #name.tick-slider-input
   updateProgress(event.target);
 }
-
-// updateProgress used to display handle on track green vs black
+// updateProgress handle on track
 function updateProgress(slider) {
-  // progressId coming from "data-progress-id" data set on each range input
   let progress = document.getElementById(slider.dataset.progressId);
-  // percent is the percent handle has traveled
   const percent = getSliderPercent(slider);
-  // giving input via data-progress-id style
   progress.style.width = percent * 100 + "%";
 }
-// this is getting the position of the handle relative to its range
+// position of handle relative to its range
 function getSliderPercent(slider) {
-  //slider AKA event.target.value
-  // console.log(event.target.value);
-  // slider.max and min are coming from range input min and max
   const range = slider.max - slider.min;
-  // slider.value is comming from intial value or what value is when handle dragged
   const absValue = slider.value - slider.min;
-  // console.log(absValue / range);
-  // returning the percent of the way the handle has traveled
-  // accross the slider
   return absValue / range;
 }
 
